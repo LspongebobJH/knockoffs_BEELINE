07da1390fc691704a8b3810ad244f1d7a55903c0
ekernf01 <eric.kern13@gmail.com>
2021-06-11 16:56:47 -0400

    Update to latest rlookc

--- Algorithms/LOOK/runLOOK.R
+++ Algorithms/LOOK/runLOOK.R
@@ -1,22 +1,45 @@
-unloadNamespace("rlookc")
-library("rlookc")
 args <- commandArgs(trailingOnly = T)
 inFile <- args[1]
 outFile <-  args[2]
 
-# input expression data
+# Input expression data
 inputExpr <- read.table(inFile, sep=",", header = 1, row.names = 1)
+inputExpr = as.matrix(inputExpr)
 geneNames <- rownames(inputExpr)
+
 rownames(inputExpr) <- c(geneNames)
+# Optional smoothing
+# neighbors = FNN::get.knn(t(inputExpr), k = 20)
+# inputExpr = sapply(seq(nrow(neighbors$nn.dist)),
+#                    function(i) rowMeans(inputExpr[,c(i, neighbors$nn.index[i,])]) )
+
+# Optional transformation to gaussian marginals
+# div_by_max = function(x) x/max(x)
+# for(i in seq(nrow(inputExpr))){
+#   inputExpr[i,] = rank( inputExpr[i,] , ties.method = "random" ) %>% div_by_max %>% qnorm
+# }
+
+# Input must be standardized
 for(i in seq(nrow(inputExpr))){
   inputExpr[i,] = inputExpr[i,] - mean(inputExpr[i,])
   inputExpr[i,] = inputExpr[i,] / (1e-8 + sd(inputExpr[i,]))
 }
+
+# Optional calibration check
+knockoffs = rlookc::computeGaussianKnockoffs(X = t(inputExpr), mu = 0, Sigma = cor(t(inputExpr)), num_realizations = 100)
+calibration_results = rlookc::simulateY(
+  X = t(inputExpr), 
+  knockoffs = knockoffs,
+  plot_savepath = paste0(outFile, "_calibration.pdf")
+)
+saveRDS(calibration_results, paste0(outFile, "_calibration.Rda"))
+
+# Core functionality: GRN inference via LOOKs
 knockoffResults = rlookc::generateLooks(
   t(inputExpr), 
   mu = 0,
-  Sigma = diag(rep(1, nrow(inputExpr))), #  0.95*cor(t(inputExpr)) + 0.05*
-  statistic = rlookc::marginal_screen,
+  Sigma = cor(t(inputExpr)), 
+  statistic = knockoff::stat.lasso_lambdasmax,
   output_type = "statistics"
 )
 
@@ -26,11 +49,15 @@ for(i in seq(nrow(inputExpr))){
     Gene1 = geneNames[ i],
     Gene2 = geneNames[-i],
     knockoff_stat = knockoffResults[[i]], 
-    q_value = rlookc::knockoff.qvals(knockoffResults[[i]])
+    q_value = rlookc::knockoffQvals(knockoffResults[[i]], offset = 0)
   )
 }
 DF = data.table::rbindlist(DF)
+# print( 
+#   ggplot(DF %>% subset(Gene1=="g15")) + 
+#     geom_point(aes(x = knockoff_stat, y = q_value, color = Gene1))
+# )
 # Write output to a file
-# https://stackoverflow.com/questions/38664241/ranking-and-counting-matrix-elements-in-r
 outDF <- DF[order(DF$q_value, decreasing=FALSE), ]
 write.table(outDF, outFile, sep = "\t", quote = FALSE, row.names = FALSE)
+warnings()
