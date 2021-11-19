12828f1ccc9306faf74ba83a5e060a938212d93c
ekernf01 <eric.kern13@gmail.com>
2021-11-19 16:20:31 -0500

    Clarify instructions to reproduce

---
+++ test_knockoffs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+# Run experiments testing model-X knockoffs for inference of the BEELINE toy networks.
+conda activate BEELINE
+./initialize.sh
+
+for toy in BFC BF CY LI LL TF
+do
+    python BLRunner.py --config config-files/knockoff_experiments/dyn-${toy}.yaml
+    python BLEvaluator.py --config config-files/knockoff_experiments/dyn-${toy}.yaml --auc
+    python BLEvaluator.py --config config-files/knockoff_experiments/dyn-${toy}.yaml --ufdr
+    python BLEvaluator.py --config config-files/knockoff_experiments/dyn-${toy}.yaml --fdr
+done
+
+Rscript plotKnockoffEvaluation.R
